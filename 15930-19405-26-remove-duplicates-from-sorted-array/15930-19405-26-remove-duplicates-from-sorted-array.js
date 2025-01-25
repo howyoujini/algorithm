61c2e4b17d2fa45be9ed72fee42c11dd6c743b26
Youjin <85492452+howyoujini@users.noreply.github.com>
2025-01-25 22:28:24 +0900

    Time: 0 ms, Memory: 52.9 MB - LeetHub

---
+++ 15930-19405-26-remove-duplicates-from-sorted-array/15930-19405-26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums - Sorted integer array
+ * @return {number} - Number of unique elements
+ */
+function removeDuplicates(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    
+    return k;
+}
