8b9b70527ac9cfd79ba5f8efac5bcace8fa40c0d
Lucy(Youjin) Hong <85492452+howyoujini@users.noreply.github.com>
2025-02-21 01:29:59 +0900

    Time: 6 ms, Memory: 57.6 MB - LeetHub

--- 10043-8506-125-valid-palindrome/10043-8506-125-valid-palindrome.js
+++ 10043-8506-125-valid-palindrome/10043-8506-125-valid-palindrome.js
@@ -2,18 +2,37 @@
  * @param {string} s
  * @return {boolean}
  */
+// O(n)
+// var isPalindrome = function (s) {
+//     let cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+//     while (cleaned.length >= 0) {
+//         if (cleaned.length === 0) {
+//             return true;
+//         }
+
+//         if (cleaned[0] === cleaned[cleaned.length - 1]) {
+//             cleaned = cleaned.slice(1, -1);
+//         } else {
+//             return false;
+//         }
+//     }
+// };
+
+
+// O(1)
 var isPalindrome = function (s) {
     let cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let left = 0;
+    let right = cleaned.length - 1;
 
-    while (cleaned.length >= 0) {
-        if (cleaned.length === 0) {
-            return true;
-        }
-
-        if (cleaned[0] === cleaned[cleaned.length - 1]) {
-            cleaned = cleaned.slice(1, -1);
-        } else {
+    while (left < right) {
+        if (cleaned[left] !== cleaned[right]) {
             return false;
         }
+        left++;
+        right--;
     }
+
+    return true;
 };
