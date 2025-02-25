f5864178dc582c0a67f3164aa00562ca32c04be6
Lucy(Youjin) Hong <85492452+howyoujini@users.noreply.github.com>
2025-02-25 22:30:49 +0900

    Time: 0 ms, Memory: 56 MB - LeetHub

---
+++ 10093-570-392-is-subsequence/10093-570-392-is-subsequence.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+    let sIndex = 0;
+    let tIndex = 0;
+
+    while (sIndex < s.length && tIndex < t.length) {
+        if (s[sIndex] === t[tIndex]) {
+            sIndex++;
+        }
+        tIndex++;
+    }
+
+    return sIndex === s.length;
+};
