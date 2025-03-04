2b2befe9d57ac16a24725d3d0b87afd7a6b40976
Lucy(Youjin) Hong <85492452+howyoujini@users.noreply.github.com>
2025-03-04 23:26:52 +0900

    Time: 3 ms, Memory: 63.4 MB - LeetHub

---
+++ 30723-1935-11-container-with-most-water/30723-1935-11-container-with-most-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
+
+    while (left < right) {
+        const width = right - left;
+        const minHeight = Math.min(height[left], height[right]);
+        const area = width * minHeight;
+
+        maxWater = Math.max(maxWater, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxWater;
+};
